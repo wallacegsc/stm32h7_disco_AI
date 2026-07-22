16e869ca0ca4fc5e49c5dec6b6edfb4a6ccd1249
wallace.gabriel <wgdsc.eng21@uea.edu.br>
2026-07-22 13:59:11 -0400

    add models

---
+++ CM7/X-CUBE-AI/App/hello_world_model_data.h
@@ -0,0 +1,81 @@
+/**
+  ******************************************************************************
+  * @file    hello_world_model_data.h
+  * @author  AST Embedded Analytics Research Platform
+  * @date    2026-07-22T13:56:33-0400
+  * @brief   AI Tool Automatic Code Generator for Embedded NN computing
+  ******************************************************************************
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  ******************************************************************************
+  */
+
+#ifndef HELLO_WORLD_MODEL_DATA_H
+#define HELLO_WORLD_MODEL_DATA_H
+
+#include "hello_world_model_config.h"
+#include "hello_world_model_data_params.h"
+
+AI_DEPRECATED
+#define AI_HELLO_WORLD_MODEL_DATA_ACTIVATIONS(ptr_)  \
+  ai_hello_world_model_data_activations_buffer_get(AI_HANDLE_PTR(ptr_))
+
+AI_DEPRECATED
+#define AI_HELLO_WORLD_MODEL_DATA_WEIGHTS(ptr_)  \
+  ai_hello_world_model_data_weights_buffer_get(AI_HANDLE_PTR(ptr_))
+
+
+AI_API_DECLARE_BEGIN
+
+
+extern const ai_u64 s_hello_world_model_weights_array_u64[161];
+
+
+
+/*!
+ * @brief Get network activations buffer initialized struct.
+ * @ingroup hello_world_model_data
+ * @param[in] ptr a pointer to the activations array storage area
+ * @return an ai_buffer initialized struct
+ */
+AI_DEPRECATED
+AI_API_ENTRY
+ai_buffer ai_hello_world_model_data_activations_buffer_get(const ai_handle ptr);
+
+/*!
+ * @brief Get network weights buffer initialized struct.
+ * @ingroup hello_world_model_data
+ * @param[in] ptr a pointer to the weights array storage area
+ * @return an ai_buffer initialized struct
+ */
+AI_DEPRECATED
+AI_API_ENTRY
+ai_buffer ai_hello_world_model_data_weights_buffer_get(const ai_handle ptr);
+
+/*!
+ * @brief Get network weights array pointer as a handle ptr.
+ * @ingroup hello_world_model_data
+ * @return a ai_handle pointer to the weights array
+ */
+AI_DEPRECATED
+AI_API_ENTRY
+ai_handle ai_hello_world_model_data_weights_get(void);
+
+
+/*!
+ * @brief Get network params configuration data structure.
+ * @ingroup hello_world_model_data
+ * @return true if a valid configuration is present, false otherwise
+ */
+AI_API_ENTRY
+ai_bool ai_hello_world_model_data_params_get(ai_network_params* params);
+
+
+AI_API_DECLARE_END
+
+#endif /* HELLO_WORLD_MODEL_DATA_H */
+
